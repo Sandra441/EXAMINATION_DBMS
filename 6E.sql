f5965fc1bc0760bb75b531d69137102207dc0230
Apolot Claire Sandra <99275622+Sandra441@users.noreply.github.com>
2025-01-17 15:15:07 +0300

    Update 6e final.sql

--- 6E.sql
+++ 6E.sql
@@ -1,8 +1,3 @@
-CREATE OR REPLACE TRIGGER Check_Limit_Stud_Sports
-BEFORE INSERT ON Stud_Sport
-FOR EACH ROW
-BEGIN
-   IF (SELECT COUNT(*) FROM Stud_Sport WHERE studentid = :NEW.studentid) >= 3 THEN
-      RAISE_APPLICATION_ERROR(-20001, 'A student can only participate in a maximum of 3 sports.');
-   END IF;
-END;
+ALTER TABLE Stud_Sport
+ADD CONSTRAINT check_sport_limit
+CHECK ((SELECT COUNT(*) FROM Stud_Sport WHERE studentid = studentid) <= 3);
